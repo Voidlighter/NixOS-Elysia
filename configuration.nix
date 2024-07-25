ca7db92d0ef29af13553b5e66d776cb8722c6b38
voidlighter <voidlighter@proton.me>
2024-07-25 14:18:32 -0600

    turned off audio so it wasn't flashing on the screen

--- configuration.nix
+++ configuration.nix
@@ -62,14 +62,14 @@
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
+  security.rtkit.enable = false;
   services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
+    enable = false;
+    alsa.enable = false;
+    alsa.support32Bit = false;
     pulse.enable = false;
     # If you want to use JACK applications, uncomment this
-    jack.enable = true;
+    jack.enable = false;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
